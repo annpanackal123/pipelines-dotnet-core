cd056d9b59f1b99d051bd67b953360ab14d0d769
annpanackal123 <147190598+annpanackal123@users.noreply.github.com>
2023-11-13 21:32:45 +0000

    Update MyTests.cs

--- MyTests.cs
+++ MyTests.cs
@@ -1,8 +1,11 @@
 using NUnit.Framework;
+
 namespace MyApp
 {
+    [TestFixture]
     public class MyTests
     {
+        [Test]
         public void MyTest()
         {
             Assert.AreEqual(1, 1);
